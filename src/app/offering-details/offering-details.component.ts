4bfce82a3d652950ae632814f95a74fa540d5273
Arunoday ray <arunoday.ray@full-cycle.com>
2018-10-07 20:43:04 +0530

    Offering Detail intial commit

--- src/app/offering-details/offering-details.component.ts
+++ src/app/offering-details/offering-details.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {OfferingDetailsService} from '../offering-details.service';
-import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-offering-details',
   templateUrl: './offering-details.component.html',
-  styleUrls: ['./offering-details.component.scss']
+  styleUrls: ['./offering-details.component.scss','../app.component.scss','../font-awesome/css/font-awesome.min.css']
 })
 export class OfferingDetailsComponent implements OnInit {
-  resp: any;
-  offeringResp: any;
 
-  constructor(private router:Router,private offeringDetails:OfferingDetailsService) { }
+
+  constructor() { }
 
   ngOnInit() {
-    this.getOfferingDetails();
+
   }
-  getOfferingDetails(): void {
-    this.offeringDetails.getOfferingData()
-      .subscribe(
-        resp => {
-          this.offeringResp = resp;
-          console.log(this.offeringResp);
-        }
-      )
-  }
+
 
 }
