2ba5d9b5a3f781806cdc88835f4e16fc87ea0581
Arunoday ray <arunoday.ray@full-cycle.com>
2018-11-19 21:08:08 +0530

    verify_document_file_uploader with ng2-file-upload

--- src/app/manage-account/verify-account/verify-account.component.ts
+++ src/app/manage-account/verify-account/verify-account.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 
+const URL = 'path_to_api';
 @Component({
   selector: 'app-verify-account',
   templateUrl: './verify-account.component.html',
   styleUrls: ['./verify-account.component.scss']
 })
 export class VerifyAccountComponent implements OnInit {
+  public uploader: FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
   }
 
 }
